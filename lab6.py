89bcff2191a481d17a6f6534fa064280f4128a14
Marchosias-0525_zenbook14 <b812110013@tmu.edu.tw>
2023-12-16 15:32:51 +0800

    bar chart

--- lab6.py
+++ lab6.py
@@ -2,6 +2,9 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from pylab import matplotlib
+from matplotlib import font_manager
+
 
 file_path = r"C:\Programming\Python\python\Lab6\cost.csv"
 
@@ -13,12 +16,21 @@ cost = pd.read_csv(file_path, encoding='utf-8')
 
 col1, col2 = st.columns(2)
 with col1:
-    city = st.multiselect('選擇想要顯示的**縣市**...', cost.columns, default=["年別"])
+    city = st.multiselect('選擇想要顯示的**縣市**...', cost.columns, default=["年別", "總平均"])
 
 with col2:
     year_range = st.slider('選擇想要顯示的**年份**...', cost['年別'].unique()[0], cost['年別'].unique()[-1], (100, 101), step=1)
     year = [i for i in range(year_range[0], year_range[1]+1, 1)]
 
 st.subheader(f'表1：民國 {year_range[0]} 至 {year_range[1]} 年', divider='grey')
-st.write(cost[cost["年別"].isin(year)][city])
+cost_select = cost[cost["年別"].isin(year)][city]
+st.write(cost_select)
+st.divider()
+
+# setup fonts for display mandarin characters
+font_file = r"C:\Kevin\font\jf-openhuninn-2.0.ttf"  # The path to the custom font file.
+font_manager.fontManager.addfont(font_file)
+matplotlib.rcParams['font.family'] = ['jf-openhuninn-2.0', 'sans-serif']
+
+st.pyplot(cost_select.plot(x='年別', kind='bar',title='長條圖').legend(bbox_to_anchor=(1.0, 1.0), fontsize='small').figure)
 st.divider()
