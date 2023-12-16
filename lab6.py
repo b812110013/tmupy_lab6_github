eda5ec6a30d4e2fbad6ac8d8b216a1234abaa255
Marchosias-0525_zenbook14 <b812110013@tmu.edu.tw>
2023-12-16 15:03:15 +0800

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -7,5 +7,5 @@ import numpy as np
 st.title("家庭收支調查：縣市平均每人月消費")
 st.markdown('[資料來源：行政院主計總處家庭收支調查](https://www.stat.gov.tw/cp.aspx?n=3914)')
 
-cost = pd.read_csv('./dataset/cost.csv', encoding='utf-8')
+cost = pd.read_csv(r'C:\Programming\Python\python\Lab6\cost.csv', encoding='utf-8')
 st.dataframe(cost)
