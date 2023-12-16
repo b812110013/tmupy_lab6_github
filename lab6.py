752fe6ef1eb4994536efcc5b1d8477af80d39749
Marchosias-0525_zenbook14 <b812110013@tmu.edu.tw>
2023-12-16 15:20:07 +0800

    cost depend on county

--- lab6.py
+++ lab6.py
@@ -3,9 +3,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+file_path = r"C:\Programming\Python\python\Lab6\cost.csv"
 
-st.title("家庭收支調查：縣市平均每人月消費")
-st.markdown('[資料來源：行政院主計總處家庭收支調查](https://www.stat.gov.tw/cp.aspx?n=3914)')
+st.title(":flag-tw: 家庭收支調查")
+st.header(':grey[縣市每人平均月消費]', divider='rainbow')
+st.markdown('[*資料來源：行政院主計總處家庭收支調查*](https://www.stat.gov.tw/cp.aspx?n=3914)')
 
-cost = pd.read_csv(r'C:\Programming\Python\python\Lab6\cost.csv', encoding='utf-8')
-st.dataframe(cost)
+cost = pd.read_csv(file_path, encoding='utf-8')
+columns = cost.columns
+options = st.multiselect('選擇想要顯示的縣市...', columns, default=["年別"])
+st.subheader(f'表1：民國 {cost["年別"].iloc[0]} 至 {cost["年別"].iloc[-1]} 年', divider='grey')
+#st.dataframe(cost[options])
+st.write(cost[options])
+st.divider()
