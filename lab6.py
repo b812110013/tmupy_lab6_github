8603bcc2b8016f835cce150869ded79a2dbc097f
Marchosias-0525_zenbook14 <b812110013@tmu.edu.tw>
2023-12-16 15:22:35 +0800

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -11,9 +11,13 @@ st.markdown('[*資料來源：行政院主計總處家庭收支調查*](https://
 
 cost = pd.read_csv(file_path, encoding='utf-8')
 
-city = st.multiselect('選擇想要顯示的**縣市**...', cost.columns, default=["年別"])
-year_range = st.slider('選擇想要顯示的**年份**...', cost['年別'].unique()[0], cost['年別'].unique()[-1], (100, 101), step=1)
-year = [i for i in range(year_range[0], year_range[1]+1, 1)]
+col1, col2 = st.columns(2)
+with col1:
+    city = st.multiselect('選擇想要顯示的**縣市**...', cost.columns, default=["年別"])
+
+with col2:
+    year_range = st.slider('選擇想要顯示的**年份**...', cost['年別'].unique()[0], cost['年別'].unique()[-1], (100, 101), step=1)
+    year = [i for i in range(year_range[0], year_range[1]+1, 1)]
 
 st.subheader(f'表1：民國 {year_range[0]} 至 {year_range[1]} 年', divider='grey')
 st.write(cost[cost["年別"].isin(year)][city])
